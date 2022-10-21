c5eca8c5757213758f7a944321a961d38e14c36b
Luke McDowell <78881317+Redskywalker7@users.noreply.github.com>
2022-10-21 14:26:25 -0600

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -50,7 +50,6 @@ def main():
                     st.write('Height: ' + str(DF.Height[0]))
                     st.write('HP: ' + str(DF.HP[0]))
                     st.write('Experience: ' + str(DF.Experience[0])) 
-                    st.write('Experience: ' + str(DF.Experience[0])) 
                     st.write('Number: ' + str(DF.id[0]))
     
 if __name__ == "__main__":
